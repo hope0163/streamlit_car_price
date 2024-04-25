2469d2f73f1762aff4eec4c4346f1d102aaee544
hope0163 <pupple2327@naver.com>
2024-04-25 11:38:20 +0900

    verson 1.0

--- eda.py
+++ eda.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
 
 def run_eda():
     st.subheader('탐색적 데이터 분석')
@@ -23,32 +25,26 @@ def run_eda():
     st.text('컬럼을 선택하면 각 컬럼별 최대/최소 데이터를 보여드립니다.')
     column_list = ['Age', 'Annual Salary', 'Credit Card Debt', 'Net Worth', 'Car Purchase Amount']
     choice_column = st.selectbox('컬럼을 선택하세요.', column_list)
-    if choice_column[0]:
-        st.text(f'{choice_column}값이 최대인 데이터')
-        st.dataframe(df.loc[ df[choice_column].max() == df[choice_column] , ])
-        st.text(f'{choice_column}값이 최소인 데이터')
-        st.dataframe(df.loc[ df[choice_column].min() == df[choice_column] , ])
+    st.info(f'선택하신 {choice_column}의 최대 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[ df[choice_column].max() == df[choice_column] , ])
+    st.info(f'선택하신 {choice_column}의 최소 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[ df[choice_column].min() == df[choice_column] , ])
 
-    elif choice_column[1]:
-        st.text(f'{choice_column}값이 최대인 데이터')
-        st.dataframe(df.loc[ df[choice_column].max() == df[choice_column] , ])
-        st.text(f'{choice_column}값이 최소인 데이터')
-        st.dataframe(df.loc[ df[choice_column].min() == df[choice_column] , ])
+    st.subheader('상관관계 분석')
+    st.text('컬럼들을 2개 이상 선택하면, 컬럼들의 상관계수를 보여드립니다.')
     
-    elif choice_column[2]:
-        st.text(f'{choice_column}값이 최대인 데이터')
-        st.dataframe(df.loc[ df[choice_column].max() == df[choice_column] , ])
-        st.text(f'{choice_column}값이 최소인 데이터')
-        st.dataframe(df.loc[ df[choice_column].min() == df[choice_column] , ])
+    corr_column_list = ['Age', 'Annual Salary', 'Credit Card Debt', 'Net Worth', 'Car Purchase Amount']
+    selected_columns = st.multiselect('컬럼을 선택하세요.', corr_column_list)
 
-    elif choice_column[3]:
-        st.text(f'{choice_column}값이 최대인 데이터')
-        st.dataframe(df.loc[ df[choice_column].max() == df[choice_column] , ])
-        st.text(f'{choice_column}값이 최소인 데이터')
-        st.dataframe(df.loc[ df[choice_column].min() == df[choice_column] , ])
+    # 2개 이상 선택했을때와 그렇지 않을때로 개발
+    if len(selected_columns) >= 2:
+        # 1. seaborn의 pairplot을 그린다.
+        # seaborn pairplot 화면에 나오지 않을 때 해결법
+        # https://discuss.streamlit.io/t/seaborn-pairplot-assistance/6738/4
+        fig1 = sb.pairplot(data=df, vars=selected_columns)
+        st.pyplot(fig1)
 
-    elif choice_column[4]:
-        st.text(f'{choice_column}값이 최대인 데이터')
-        st.dataframe(df.loc[ df[choice_column].max() == df[choice_column] , ])
-        st.text(f'{choice_column}값이 최소인 데이터')
-        st.dataframe(df.loc[ df[choice_column].min() == df[choice_column] , ])
+        # 2. 상관계수를 보여준다.
+        st.dataframe(df[selected_columns].corr())
+    else:
+        st.warning('컬럼은 2개 이상 선택해야합니다.')
--- ml.py
+++ ml.py
@@ -1,4 +1,52 @@
+import math
+import joblib
+import numpy as np
 import streamlit as st
 
 def run_ml():
-    st.subheader("머신러닝")
+    st.subheader("자동차 구매 가격 예측하기")
+
+    # 1. 예측하기 위해서, 유저한테 입력을 받는다.
+    # Gender, Age, Annual Salary, Credit Card Debt, Net Worth => 예측에 필요한 값들
+
+    st.text('성별을 선택하세요.')
+    gender = st.radio('성별 선택', ['남자', '여자'])
+    if gender == '남자':
+         gender = 1
+    else:
+         gender = 0
+    
+    st.text('나이를 입력하세요.')
+    age = st.number_input('나이 입력', min_value=18, max_value=100, value=24)
+
+    st.text('연봉을 입력하세요.')
+    salary = st.number_input('연봉 입력', min_value=10000, value=50000, step=1000)
+
+    st.text('카드 빚을 입력하세요.')
+    debt = st.number_input('카드 빚 입력', min_value=0, value=1000, step=1000)
+
+    st.text('자산을 입력하세요.')
+    worth = st.number_input('자산 입력', min_value=5000, value=20000, step=1000)
+
+    st.subheader('버튼을 누르면 예측합니다.')
+
+    if st.button('예측하기'):
+          # 2. 예측한다.
+
+          # 2-1. 모델이 있어야 한다.
+          regressor = joblib.load('./model/regressor.pkl')
+          print(regressor)
+
+          # 2-2. 유저가 입력한 데이터를 모델에 예측할 수 있도록 가공해야 한다.
+          new_data = [gender, age, salary, debt, worth]
+          print(new_data)
+          print(np.array(new_data).reshape(1,-1))
+          
+          new_data = np.array(new_data).reshape(1,-1)
+          
+          # 2-3. 모델의 predict 함수로 예측한다.
+          y_pred = regressor.predict(new_data)
+          print(y_pred)
+
+          # 위의 데이터로 예측한 자동차 구매 가능 금액은 6,746달러 입니다. => 유저에게 표시
+          st.info(f'위의 데이터로 예측한 자동차 구매 가능 금액은 {format(int(y_pred), ',')}달러 입니다.')
